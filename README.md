621643361a20f0dc3a1a73527b59e4c167d5bc71
alantrrs <alan.trrs.ngls@gmail.com>
2015-10-21 21:30:34 -0700

    download files

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# Dataset.js
+This library defines a common API for installing and interfacing with datasets.
+The idea is that using a dataset should be as simple as requiring it into your project.
+
+## Install
+The first step is getting the resources. These resources are usually compressed, so the second 
+step is decompressing them into the proper file structure.
--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 // TODO: Download data
+// TODO: Decompress
 // TODO: Define interfaces
+
+var fetch = require('node-fetch')
+
+var fs = require('mz/fs')
+
+var resources = [
+  'http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCdevkit_08-Jun-2007.tar',
+  'http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtestnoimgs_06-Nov-2007.tar'
+]
+
+function download(source, destination) {
+  return new Promise(function(resolve, reject) {
+    return fetch(source).then(function(response){
+      // TODO: calculate based on hash?
+      var outputPath = destination + source.split('/').pop()
+      var out = fs.createWriteStream(outputPath)
+      response.body.pipe(out).on('finish',function(){
+        resolve(outputPath)
+      }).on('error',reject)
+    }).catch(reject)
+  })
+}
+const TMP_DIR = './'
+
+// EXECUTE
+Promise.all(resources.map(function(r){
+  return download(r,TMP_DIR)
+})).then(function(paths){
+  console.log('files:', paths)
+}).catch(function(err){
+  console.error(err)
+})
