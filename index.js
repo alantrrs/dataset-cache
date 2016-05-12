c2a722300c0d5efcf3ffa33e88ad894a7303a2cc
alantrrs <alan.trrs.ngls@gmail.com>
2016-05-12 01:05:38 -0700

    Get single files

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-// TODO: Function to generate a json file based on a dataset
-// TODO: Download data
-// TODO: Decompress
-// TODO: Define interfaces
-
 var fetch = require('node-fetch')
-var fs = require('mz/fs')
-var md5 = require('md5')
-var mkdirp = require('mkdirp')
+var fs = require('fs')
+var checksum = require('checksum')
+var shortid = require('shortid')
+var path = require('path')
 
-function unique_path (data, base_path) {
-  return base_path + md5(data)
-}
-
-function download (source, destination) {
+function download (source, data_dir) {
   return new Promise(function (resolve, reject) {
     return fetch(source).then(function (response) {
-      // TODO: calculate based on hash?
-      var outputPath = destination + source.split('/').pop()
+      var outputPath = path.join(data_dir, source.split('/').pop() + shortid.generate() + '.tmp')
       var out = fs.createWriteStream(outputPath)
       response.body.pipe(out).on('finish', function () {
         resolve(outputPath)
@@ -25,15 +16,85 @@ function download (source, destination) {
   })
 }
 
-// TODO: Cache
+function validateCache (data_dir, source_hash) {
+  var cached_path = path.join(data_dir, source_hash)
+  return hashFile(cached_path).then(function (hash) {
+    var valid = (hash === source_hash)
+    return {
+      path: cached_path,
+      hash: hash,
+      valid: valid,
+      cached: valid
+    }
+  })
+}
 
-// EXECUTE
-const DATA_DIR = process.env.SCIHUB_DATA_DIR || '/tmp/'
-exports.install = function (config) {
-  var out_dir = unique_path(config, DATA_DIR) + '/'
-  // Create unique directory
-  mkdirp.sync(out_dir)
-  return Promise.all(config.resources.map(function (r) {
-    return download(r, out_dir)
+function get (source, data_dir) {
+  // Check if the data exists and is valid
+  return validateCache(data_dir, source.hash).then(function (data) {
+    if (data.cached) return data
+    // Download
+    return download(source.url, data_dir)
+    // Get checksum
+    .then(function (file) {
+      return hashFile(file).then(function (hash) {
+        return {
+          path: file,
+          hash: hash
+        }
+      })
+    })
+    // Verify against original hash
+    .then(function (newFile) {
+      newFile.valid = false
+      if (newFile.hash === source.hash) {
+        newFile.valid = true
+      }
+      return newFile
+    })
+    // Move file to final data_dir
+    .then(function (newFile) {
+      const new_path = path.join(data_dir, newFile.hash)
+      fs.renameSync(newFile.path, new_path)
+      newFile.path = new_path
+      return newFile
+    })
+  })
+}
+
+function hashFile (file) {
+  return new Promise(function (resolve, reject) {
+    checksum.file(file, {algorithm: 'sha256'}, function (err, sum) {
+      if (err) {
+        // If the file doesn't exist return null
+        if (err.code === 'ENOENT') return resolve(null)
+        return reject(err)
+      }
+      resolve(sum)
+    })
+  })
+}
+
+/*
+function hashDir (dir) {
+  // TODO: Try this? : http://unix.stackexchange.com/questions/35832/how-do-i-get-the-md5-sum-of-a-directorys-contents-as-one-sum
+  // TODO: Parse directory
+  // TODO: Sort the files
+  // TODO: Hash of hashes?
+}
+*/
+
+/**
+ * Installs datasets from a dataset.json
+ * @param {object} config - the dataset.json contents
+ * @param {string} out_dir - destination directory
+ */
+function install (config, out_dir) {
+  if (!out_dir) out_dir = '/tmp/'
+  return Promise.all(config.resources.map(function (resource) {
+    return get(resource, out_dir)
   }))
 }
+
+exports.get = get
+exports.install = install
--- test/index.js
+++ test/index.js
@@ -1,23 +1,43 @@
-/*global describe, it */
+/* eslint-env mocha */
 
 var assert = require('assert')
-var fs = require('mz/fs')
+var fs = require('fs')
 
 var dataset = require('..')
 
 var test_data = {
   resources: [
-    'http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCdevkit_08-Jun-2007.tar',
-    'http://host.robots.ox.ac.uk/pascal/VOC/voc2007/devkit_doc_07-Jun-2007.pdf'
+    {
+      url: 'https://raw.githubusercontent.com/alantrrs/py-faster-rcnn/master/README.md',
+      hash: '9f5e6899095e366f25a612e56d8b4ebd32c6914288991e3a8a48265c38e6b6cb'
+    },
+    {
+      url: 'https://raw.githubusercontent.com/alantrrs/py-faster-rcnn/master/Dockerfile',
+      hash: '370a909aff0388e235c54eba9b962bb973ba82ac2008fdf6e9277b14ee3394fb'
+    },
+    {
+      url: 'https://raw.githubusercontent.com/alantrrs/py-faster-rcnn/master/README.md',
+      hash: 'abcd6899095d366f25a612e56d8b4ebd32c6914288991e3a8a48265c38e6b6cb' // invalid hash
+    }
   ]
 }
 
 describe('Install', function () {
-  it('should download the data', function (done) {
-    this.timeout(60000)
-    dataset.install(test_data).then(function (filepaths) {
-      filepaths.forEach(function (path) {
-        assert(fs.lstatSync(path).isFile)
+  it('should download and cache the data', function (done) {
+    this.timeout(30000)
+    dataset.install(test_data).then(function (files) {
+      files.forEach(function (file) {
+        assert(fs.lstatSync(file.path).isFile())
+      })
+      done()
+    }).catch(done)
+  })
+  it('should have cached valid objects', function (done) {
+    this.timeout(30000)
+    dataset.install(test_data).then(function (files) {
+      files.forEach(function (file) {
+        assert(fs.lstatSync(file.path).isFile())
+        if (file.valid) assert(file.cached)
       })
       done()
     }).catch(done)
