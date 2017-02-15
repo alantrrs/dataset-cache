ad5648af7229640b4f8684e6ce8c1558d6e8e72e
alantrrs <alan.trrs.ngls@gmail.com>
2017-02-15 00:22:35 -0800

    Remove nesting datasets under 'resources'

--- test/index.js
+++ test/index.js
@@ -7,19 +7,17 @@ var os = require('os')
 var dataset = require('..')
 
 var test_data = {
-  resources: {
-    'my-file.txt': {
-      url: 'https://raw.githubusercontent.com/empiricalci/fixtures/master/my-file.txt',
-      hash: '8b4781a921e9f1a1cb5aa3063ca8592cac3ee39276d8e8212b336b6e73999798'
-    },
-    'another_file.txt': {
-      url: 'https://raw.githubusercontent.com/empiricalci/fixtures/master/another_file.txt',
-      hash: '0f431dfe11d428f809edb32a31867be71e753f9203945676cbc77c514c7324bf'
-    },
-    'invalid_file': {
-      url: 'https://raw.githubusercontent.com/empiricalci/fixtures/another_file.txt',
-      hash: 'abcd6899095d366f25a612e56d8b4ebd32c6914288991e3a8a48265c38e6b6cb' // invalid hash
-    }
+  'my-file.txt': {
+    url: 'https://raw.githubusercontent.com/empiricalci/fixtures/master/my-file.txt',
+    hash: '8b4781a921e9f1a1cb5aa3063ca8592cac3ee39276d8e8212b336b6e73999798'
+  },
+  'another_file.txt': {
+    url: 'https://raw.githubusercontent.com/empiricalci/fixtures/master/another_file.txt',
+    hash: '0f431dfe11d428f809edb32a31867be71e753f9203945676cbc77c514c7324bf'
+  },
+  'invalid_file': {
+    url: 'https://raw.githubusercontent.com/empiricalci/fixtures/another_file.txt',
+    hash: 'abcd6899095d366f25a612e56d8b4ebd32c6914288991e3a8a48265c38e6b6cb' // invalid hash
   }
 }
 
@@ -34,10 +32,10 @@ describe('Install files', function () {
         var file = files[k]
         assert(fs.lstatSync(file.path).isFile())
         if (k !== 'invalid_file') { // valid files
-          assert.equal(file.hash, test_data.resources[k].hash)
+          assert.equal(file.hash, test_data[k].hash)
           assert(file.valid)
         } else {
-          assert.notEqual(file.hash, test_data.resources[k].hash)
+          assert.notEqual(file.hash, test_data[k].hash)
         }
       })
       done()
@@ -52,10 +50,10 @@ describe('Install files', function () {
         assert(fs.lstatSync(file.path).isFile())
         if (k !== 'invalid_file') { // valid files
           assert(file.valid)
-          assert.equal(file.hash, test_data.resources[k].hash)
+          assert.equal(file.hash, test_data[k].hash)
           assert(file.cached)
         } else {
-          assert.notEqual(file.hash, test_data.resources[k].hash)
+          assert.notEqual(file.hash, test_data[k].hash)
           assert.equal(file.cached, false)
         }
       })
@@ -65,12 +63,10 @@ describe('Install files', function () {
 })
 
 var test_dirs = {
-  resources: {
-    'my-files': {
-      url: 'https://github.com/empiricalci/fixtures/raw/master/my-files.tar.gz',
-      hash: '0e4710c220e7ed2d11288bcf3cf111ac01bdd0cb2a4d64f81455c5b31f1a4fbe',
-      directory: true
-    }
+  'my-files': {
+    url: 'https://github.com/empiricalci/fixtures/raw/master/my-files.tar.gz',
+    hash: '0e4710c220e7ed2d11288bcf3cf111ac01bdd0cb2a4d64f81455c5b31f1a4fbe',
+    directory: true
   }
 }
 
@@ -81,7 +77,7 @@ describe('Install tarballs', function () {
       var dir = dirs['my-files']
       assert(fs.lstatSync(dir.path).isDirectory())
       assert(dir.valid)
-      assert.equal(dir.hash, test_dirs.resources['my-files'].hash)
+      assert.equal(dir.hash, test_dirs['my-files'].hash)
       done()
     }).catch(done)
   })
@@ -91,7 +87,7 @@ describe('Install tarballs', function () {
       var dir = dirs['my-files']
       assert(fs.lstatSync(dir.path).isDirectory())
       assert(dir.valid)
-      assert.equal(dir.hash, test_dirs.resources['my-files'].hash)
+      assert.equal(dir.hash, test_dirs['my-files'].hash)
       assert(dir.cached)
       done()
     }).catch(done)
